b3f28c70d9d4e147cc7515eaaf4c674307a2a147
Kribble <92189346+Cheesechicken2@users.noreply.github.com>
2024-08-29 16:34:02 -0500

    forgot daytimemanager

---
+++ Assets/IntruderMM/Scripts/DayTimeManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTimeManager : MonoBehaviour
+{
+    public bool randomTime = true;
+
+    public int[] randomTimeIndexChances;
+
+}
